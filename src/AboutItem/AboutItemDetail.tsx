7fd149f93ee411db8cdedab8248de60c85206166
Archan S Y <57443583+archanyogeesh@users.noreply.github.com>
2020-05-19 13:37:53 +0530

    Revert "feat(print_cart): merged items_core_changes branch"
    This reverts commit 609a7cbaeca34fd89e1d09bb2398f3bffd196926, reversing
    changes made to d3b296d5cb3545ed4bf31433fa8b0d24bd8a83d3.

--- src/AboutItem/AboutItemDetail.tsx
+++ src/AboutItem/AboutItemDetail.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { AboutItemModel } from "./AboutItemModels";
-import { getContentStandardCode } from "@src/ItemCard/ItemContentStandardHelper";
 
 export class AboutThisItemDetail extends React.Component<AboutItemModel, {}> {
   renderField(
@@ -21,43 +20,17 @@ export class AboutThisItemDetail extends React.Component<AboutItemModel, {}> {
   }
 
   render() {
-    /**
-     * check if Content Standards is undefined, null or blank
-     *
-     */
-
-    const subjectCode = this.props.itemCardViewModel.subjectCode;
-    const claimCode = this.props.itemCardViewModel.claimCode;
-    let commonCoreStandardId = this.props.itemCardViewModel
-      .commonCoreStandardId;
-    let ccssDescription = this.props.itemCardViewModel.ccssDescription;
-
-    //get the new and logically updated commonCoreStandardId, ccssDescription value
-    const standard = getContentStandardCode(
-      subjectCode,
-      claimCode,
-      commonCoreStandardId,
-      ccssDescription
-    );
-    commonCoreStandardId = standard["commonCoreStandardId"];
-    ccssDescription = standard["ccssDescription"];
-
     return (
       <div className={"item-details"}>
         {this.renderField(
-          "Item Id",
-          "item-id",
-          this.props.itemCardViewModel.itemKey
+          "Subject",
+          "subject",
+          this.props.itemCardViewModel.subjectLabel
         )}
         {this.renderField(
           "Grade",
           "grade",
           this.props.itemCardViewModel.gradeLabel
-        )}
-        {this.renderField(
-          "Subject",
-          "subject",
-          this.props.itemCardViewModel.subjectLabel
         )}
         {this.renderField(
           "Claim",
@@ -70,26 +43,30 @@ export class AboutThisItemDetail extends React.Component<AboutItemModel, {}> {
           this.props.itemCardViewModel.targetShortName
         )}
         {this.renderField(
-          "Target Description",
-          "target-description",
-          this.props.targetDescription
-        )}
-        {this.renderField(
-          "Content Standard",
-          "ccss",
-          commonCoreStandardId + ". " + ccssDescription
-        )}
-        {this.renderField(
           "Item Type",
           "interaction-type",
           this.props.itemCardViewModel.interactionTypeLabel
+        )}
+        {this.renderField(
+          "Item Id",
+          "item-id",
+          this.props.itemCardViewModel.itemKey
         )}
         {this.renderField(
           "Depth of Knowledge",
           "dok",
           this.props.depthOfKnowledge
         )}
-
+        {this.renderField(
+          "Common Core State Standard",
+          "ccss",
+          this.props.commonCoreStandardsDescription
+        )}
+        {this.renderField(
+          "Target Description",
+          "target-description",
+          this.props.targetDescription
+        )}
         {this.renderField(
           "Educational Difficulty",
           "educational-difficulty",
