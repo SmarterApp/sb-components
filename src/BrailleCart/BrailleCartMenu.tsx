6b1d648caafdd2259ea3d61c3bc3a754afaffc4e
Archan S Y <57443583+archanyogeesh@users.noreply.github.com>
2020-08-28 17:34:11 +0530

    feat(braille_cart): remove braille options when unselected
     remove braille options when unselected

--- src/BrailleCart/BrailleCartMenu.tsx
+++ src/BrailleCart/BrailleCartMenu.tsx
@@ -19,19 +19,32 @@ export class BrailleCartMenu extends React.Component<BrailleCartMenuProps> {
     };
   }
 
-  onMusltiSelect = (v: MultiselectValue[]) => {};
-
   handleChangedBrailleType = (v: MultiselectValue[]) => {
-    //change in item itself
-    if (this.props.item !== undefined) {
-      if (this.props.item.selectedBrailleTypes === undefined)
-        this.props.item.selectedBrailleTypes = [];
-      if (this.props.item.selectedBrailleTypes.indexOf(v[0].value) === -1) {
-        this.props.item.selectedBrailleTypes.push(v[0].value);
-        console.log(
-          "****hahahahahhaha finally>>>>",
-          this.props.item.selectedBrailleTypes
-        );
+    //Check if option get selected
+    if (v[0].selected && v[0].selected === true) {
+      if (this.props.item !== undefined) {
+        if (this.props.item.selectedBrailleTypes === undefined)
+          this.props.item.selectedBrailleTypes = [];
+        if (this.props.item.selectedBrailleTypes.indexOf(v[0].value) === -1) {
+          this.props.item.selectedBrailleTypes.push(v[0].value);
+        }
+      }
+    } else {
+      //else - option is unselected
+      if (this.props.item !== undefined) {
+        if (this.props.item.selectedBrailleTypes !== undefined) {
+          if (this.props.item.selectedBrailleTypes.indexOf(v[0].value) !== -1) {
+            const filteredSelectedBrailleType = this.props.item.selectedBrailleTypes.filter(
+              option => option != v[0].value
+            );
+            this.props.item.selectedBrailleTypes = filteredSelectedBrailleType;
+          }
+          //check if selected Braille array for an item is empty,
+          // make it undefined if it is empty
+          if (this.props.item.selectedBrailleTypes.length <= 0) {
+            this.props.item.selectedBrailleTypes = undefined;
+          }
+        }
       }
     }
   };
